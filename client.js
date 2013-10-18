0b809a5a1b8ea11bd52d0590423248555d5f1a8f
Nicolas Herment <nherment@arkhaios.net>
2013-10-18 17:28:34 +0100

    better test

--- client.js
+++ client.js
@@ -3,7 +3,11 @@ describe('jquery', function() {
   it("should work", function(done) {
 
     $.get('http://localhost:3000/hello.txt', function(data) {
-      done();
+      if(data === 'Hello World') {
+        done();
+      } else {
+        done(new Error("data not as expected: "+data))
+      }
     })
 
   })
